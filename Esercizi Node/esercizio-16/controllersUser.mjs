2f4a0af27ac5122468fc24c145f53d495650a891
Alessandro <al3ss4ndr@hotmail.it>
2023-07-24 17:47:44 +0200

    exe 16

---
+++ Esercizi Node/esercizio-16/controllersUser.mjs
@@ -0,0 +1,26 @@
+import * as dotenv from 'dotenv'
+import { db } from "./db.mjs";
+dotenv.config()
+import jwt from 'jsonwebtoken'
+
+const logIn = async (req, res) => {
+    const { username, password } = req.body
+
+    const user = await db.one(`SELECT * FROM users WHERE username=$1`, [username])
+
+    if (user && user.password === password) {
+        const payload = {
+            id: user.id,
+            username,
+        };
+        const { SECRET = "" } = process.env
+        const token = jwt.sign(payload, SECRET)
+
+        await db.none(`UPDATE users SET token=$2 WHERE id=$1`, [user.id, token])
+        res.status(200).json({ id: user.id, username, token })
+    } else {
+        res.status(400).json({ msg: "Username or password incorrect" })
+    }
+}
+
+export { logIn }
